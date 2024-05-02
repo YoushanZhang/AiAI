0b012e5d2ed09d23a9df90ef7f6cbaff0e131d41
Harsha <harsha.koduri123@gmail.com>
2024-05-02 02:11:11 -0400

    Added Voice Cloning

---
+++ Voice Cloning/VC_MODEL1/loss_function.py
@@ -0,0 +1,19 @@
+from torch import nn
+
+
+class ModelLoss(nn.Module):
+    def __init__(self):
+        super(ModelLoss, self).__init__()
+
+    def forward(self, model_output, targets):
+        mel_target, gate_target = targets[0], targets[1]
+        mel_target.requires_grad = False
+        gate_target.requires_grad = False
+        gate_target = gate_target.view(-1, 1)
+
+        mel_out, mel_out_postnet, gate_out, _ = model_output
+        gate_out = gate_out.view(-1, 1)
+        mel_loss = nn.MSELoss()(mel_out, mel_target) + \
+            nn.MSELoss()(mel_out_postnet, mel_target)
+        gate_loss = nn.BCEWithLogitsLoss()(gate_out, gate_target)
+        return mel_loss + gate_loss
